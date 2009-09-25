718417315027a953ca3ffe03c41b9ce70f3acc9b
Alejandro Eduardo Cruz Paz <spalatino@Macintosh.gateway.2wire.net>
2009-09-25 01:39:46 -0500

    Métodos auxiliares agregados a application_helper.rb : lista_de_mini_blogs y logeado_administrador?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  # Método auxiliar que hace disponibles a las instancias de mini-blog en el template application.html.erb
+  def lista_de_mini_blogs
+    MiniBlog.find(:all)
+  end  
+  
+  # Método auxiliar a nivel template para decidir que mostrar a los usuarios que son administrador
+  def logeado_administrador?
+    current_user && current_user.es_administrador?
+  end
 end
